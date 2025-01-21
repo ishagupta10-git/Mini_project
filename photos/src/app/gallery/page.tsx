b0a3bb41955c92ac83df8a6e6b340860e60b8077
isha <ishag7429@gmail.com>
2025-01-21 08:47:02 +0530

    navbar added

---
+++ photos/src/app/gallery/page.tsx
@@ -0,0 +1,30 @@
+export default function GalleryPage(){
+    return(
+        <h1>Gallery</h1>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//31:40
--- photos/src/app/page.tsx
+++ photos/src/app/page.tsx
@@ -1,12 +1,36 @@
 "use client";
+import { useState } from 'react';
 import { CldUploadButton } from 'next-cloudinary';
-import Image from "next/image";
+import { CldImage } from 'next-cloudinary';
+import { CloudinaryUploadWidgetResults, CloudinaryUploadWidgetInfo } from 'next-cloudinary';
 
 export default function Home() {
+  const [imageId, setImageId] = useState("");
+
+  const handleUpload = (results: CloudinaryUploadWidgetResults) => {
+    const info = results.info as CloudinaryUploadWidgetInfo;
+    if (results.event === 'success' && info.public_id) {
+      setImageId(info.public_id);
+    } else {
+      console.error("Upload failed or no public_id found.");
+    }
+  };
+
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <CldUploadButton uploadPreset='bhvdva9u'/>
-       </main>
-    
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <CldUploadButton
+        onUpload={handleUpload}
+        uploadPreset='bhvdva9u'
+      />
+      {imageId && (
+        <CldImage
+          width="400"
+          height="300"
+          src={imageId} // Use the dynamic imageId here
+          sizes="100vw"
+          alt="Description of my image"
+        />
+      )}
+    </main>
   );
 }
