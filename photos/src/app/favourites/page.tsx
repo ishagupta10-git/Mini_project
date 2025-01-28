b0257e30b993ce2368971a550bd1f2da0e2c69dc
komal <komal074951@gmail.com>
2025-01-28 09:32:13 +0530

    gallery and favourites page

--- photos/src/app/favourites/page.tsx
+++ photos/src/app/favourites/page.tsx
@@ -1,37 +1,30 @@
-import { CloudinaryImage } from "./cloudinary-image";
-import UploadButton from "./upload-button";
+import { CloudinaryImage } from "../gallery/cloudinary-image";
 import cloudinary from "cloudinary";
+import { SearchResult } from "../gallery/page";
+import { ForceRefresh } from "@/components/ui/force-refresh";
 
-export type SearchResult ={
-  public_id: string
-  tags: string[]
-}
-
-export default async function GalleryPage() {
+export default async function FavouritesPage() {
 
   const results = (await cloudinary.v2.search
-    .expression("resource_type:image")
+    .expression("resource_type:image AND tags=favourite")
     .sort_by("created_at","desc")
     .with_field("tags")
     .max_results(20)
     .execute()) as{resources :SearchResult[]};
 
-  console.log("results", results)
-
-
 
   return (
     <section>
+      <ForceRefresh />
       <div className="flex flex-col gap-8">
         <div className="flex justify-between">
-          <h1 className="text-4xl font-bold">Gallery</h1>
-          <UploadButton />
+          <h1 className="text-4xl font-bold">Favourite Images</h1>
         </div>
 
         <div className="grid grid-cols-4 gap-4">
           {results.resources.map((result) => (
             <CloudinaryImage
-              path="/gallery"
+              path="/favourites"
               key={result.public_id}
               imageData={result}
               width="400"
--- photos/src/app/page.tsx
+++ photos/src/app/page.tsx
@@ -1,43 +1,50 @@
 "use client";
 
-import { CldImage } from "next-cloudinary";
-import { CldUploadButton } from "next-cloudinary";
-import { useState } from "react";
+import { CldUploadButton } from 'next-cloudinary';
+import { CldImage } from 'next-cloudinary';
+import { useState } from 'react';
 
-export type UploadResult = {
-  info: {
+type UploadResult = {
+  info:{
     public_id: string;
-  };
-  event: "success";
-};
+
+  },
+  event:'success',
+}
 
 export default function Home() {
   const [imageId, setImageId] = useState("");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CldUploadButton
-        onSuccess={(results: any) => {
+    
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <CldUploadButton
+        onSuccess={(results: any) =>{
           if (results.event === "success") {
             setImageId(results.info.public_id);
           }
-        }}
-        uploadPreset="bhvdva9u"
-      />
+         
 
-      {imageId && (
-        <CldImage
-          width="500"
-          height="300"
-          src={imageId}
-          sizes="100vw"
-          alt="Description of my image"
-        />
-      )}
+        }}
+        
+        uploadPreset="upload" />
+
+        {imageId && (
+          <CldImage
+            width="400"
+            height="300"
+            src={imageId}
+            sizes="100vw"
+            blurFaces
+            
+            alt="Description of my image"
+            priority
+          />
+        )}
+
+       
     </main>
   );
 }
-
-
 
 
